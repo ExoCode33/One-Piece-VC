22ac4febf0c833d715a0b253ba9456e7955c9aa4
ExoCode33 <emmanuel@supracode.ca>
2025-06-09 19:28:39 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     token: process.env.DISCORD_TOKEN,
     clientId: process.env.CLIENT_ID,
-    createChannelName: process.env.CREATE_CHANNEL_NAME || '🏴☠ Set Sail Together',
+    createChannelName: process.env.CREATE_CHANNEL_NAME || '🏴 Set Sail Together',
     categoryName: process.env.CATEGORY_NAME || '🌊 Grand Line Voice Channels',
     deleteDelay: parseInt(process.env.DELETE_DELAY) || 5000,
     debug: process.env.DEBUG === 'true'
